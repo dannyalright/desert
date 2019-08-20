87ef815e26032ea34b7940b4fca242671e6d5ecb
Danny White <danny@dannywhite.site>
2019-08-20 15:54:33 -0400

    working two maps

--- site/map.js
+++ site/map.js
@@ -2,7 +2,18 @@
 const cornerTopLeft = L.latLng(48.16, -124.0);
 const cornerBottomRight = L.latLng(24.28, -66.44);
 const bounds = L.latLngBounds(cornerTopLeft, cornerBottomRight);
-const map = L.map("map", {
+
+const mapOne = L.map("mapOne", {
+  zoomControl: false,
+  attributionControl: false,
+  scrollWheelZoom: false,
+  boxZoom: false,
+  doubleClickZoom: false,
+  scrollWheelZoom: false,
+  dragging: false
+}).fitBounds(bounds);
+
+const mapTwo = L.map("mapTwo", {
   zoomControl: false,
   attributionControl: false,
   scrollWheelZoom: false,
@@ -17,10 +28,22 @@ L.tileLayer(
   {
     maxZoom: 18
   }
-).addTo(map);
+).addTo(mapOne);
+
+L.tileLayer(
+  "https://{s}.basemaps.cartocdn.com/rastertiles/voyager_nolabels/{z}/{x}/{y}.png",
+  {
+    maxZoom: 18
+  }
+).addTo(mapTwo);
 
 // Set up Carto API
-const client = new carto.Client({
+const clientOne = new carto.Client({
+  apiKey: "default_public",
+  username: "jleland00"
+});
+
+const clientTwo = new carto.Client({
   apiKey: "default_public",
   username: "jleland00"
 });
@@ -31,32 +54,22 @@ const client = new carto.Client({
 // });
 
 // Set intial 2000 aridity map polygons
-const source = new carto.source.SQL("SELECT * FROM table_2000_aridity");
-const style = new carto.style.CartoCSS(`
+const source2000 = new carto.source.SQL("SELECT * FROM table_2000_aridity");
+const style2000 = new carto.style.CartoCSS(`
     #layer {
       polygon-fill: ramp([column_2000_aridity_zone], (#52c406, #ffd52b, #268455, #ec650a, #73AF48, #e41b04, #666666), ("Dry Subhumid", "Semi-Arid", "Humid", "Arid", null, "Hyperarid"), "=");
     }
   `);
-const aridityLayer = new carto.layer.Layer(source, style);
+const aridityLayer2000 = new carto.layer.Layer(source2000, style2000);
 
-// Talk to checkboxes
-function set2000() {
-  source.setQuery("SELECT * FROM table_2000_aridity");
-  style.setContent(`
+//
+const source2060 = new carto.source.SQL("SELECT * FROM table_2060_aridity");
+const style2060 = new carto.style.CartoCSS(`
     #layer {
-      polygon-fill: ramp([column_2000_aridity_zone], (#52c406, #ffd52b, #268455, #ec650a, #73AF48, #e41b04, #666666), ("Dry Subhumid", "Semi-Arid", "Humid", "Arid", null, "Hyperarid"), "=");
+        polygon-fill: ramp([column_2060_projection], (#ffe600, #ff6701, #42c709, #128125, #87C55F, #e73600, #B3B3B3), ("Semi-Arid", "Arid", "Dry Subhumid", "Humid", null, "Hyperarid"), "=");
     }
   `);
-}
-
-function set2060() {
-  source.setQuery("SELECT * FROM table_2060_aridity");
-  style.setContent(`
-    #layer {
-      polygon-fill: ramp([column_2060_projection], (#52c406, #ffd52b, #268455, #ec650a, #73AF48, #e41b04, #666666), ("Dry Subhumid", "Semi-Arid", "Humid", "Arid", null, "Hyperarid"), "=");
-    }
-  `);
-}
+const aridityLayer2060 = new carto.layer.Layer(source2060, style2060);
 
 // Set state outlines
 const outlineSource = new carto.source.SQL(
@@ -74,13 +87,16 @@ const outlineStyle = new carto.style.CartoCSS(`
     line-opacity: 1;
   }
   `);
-const outlineLayer = new carto.layer.Layer(outlineSource, outlineStyle);
 
 // Add aridity layer to map
-client.addLayer(aridityLayer);
-
-// Add outline layer to map
-client.addLayer(outlineLayer);
+clientOne.addLayer(aridityLayer2000);
+clientOne.addLayer(aridityLayer2000);
 
 // Add map to page
-client.getLeafletLayer().addTo(map);
+clientOne.getLeafletLayer().addTo(mapOne);
+
+// Add aridity layer to map
+clientTwo.addLayer(aridityLayer2060);
+
+// Add map to page
+clientTwo.getLeafletLayer().addTo(mapTwo);
